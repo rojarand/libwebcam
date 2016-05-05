f5043450bf57ef973c590d925dd036addd66d326
robcio83 <zebul@o2.pl>
2016-05-05 17:13:30 +0000

    added build scripts for example programs

---
+++ examples/grabber/cmake.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+BUILD_DIR="build"
+
+do_clean() {
+	rm -rf $BUILD_DIR
+}
+
+do_build() {
+	mkdir $BUILD_DIR; cd $BUILD_DIR
+	cmake -DCMAKE_BUILD_TYPE=Debug ..
+	make # VERBOSE=1
+}
+
+do_install() {
+	cd $BUILD_DIR
+	make install
+}
+
+case $1 in
+clean)
+	do_clean
+	;;
+build)
+	do_build
+	;;
+install)
+	do_install
+	;;
+*)
+	echo "Usage: $0 { clean | build | install }"
+	exit 1
+	;;
+esac
