0fba11c9e8c6067c2c5706fdd89c45de9cd9c5f6
Peter Abeles <peter.abeles@gmail.com>
2018-10-10 16:36:30 -0700

    blah

--- libwebcam-jni/src/main/cpp/Webcam.cpp
+++ libwebcam-jni/src/main/cpp/Webcam.cpp
@@ -1,18 +1,59 @@
 #include "Webcam.h"
 #include <libwebcam/webcam.h>
 #include <stdexcept>
+#include <iostream>
 
-#include <stdio.h>
+using namespace std;
 
 extern "C" {
 
-JNIEXPORT void JNICALL Java_libwebcam_Webcam_lookupDevices(JNIEnv *env, jobject obj, jobject devices)
+jclass safe_FindClass( JNIEnv *env, const char* name )
 {
-    const webcam::DeviceInfoEnumeration & enumeration = webcam::enumerator::enumerate();
+    jclass theclass = env->FindClass(name);
+    if( env->ExceptionCheck() || theclass == nullptr ) {
+        env->ExceptionDescribe();
+        throw "failed FindClass";
+    }
+    return theclass;
+}
 
-    const int count = enumeration.count();
+jmethodID safe_GetMethodID( JNIEnv *env, jclass& objClass, const char* name , const char* type)
+{
+    jmethodID fid = env->GetMethodID(objClass, name, type);
+    if( env->ExceptionCheck() ) {
+        env->ExceptionDescribe();
+        throw "failed GetMethodID";
+    }
+    return fid;
+}
 
-    printf("Hello JNI! found=%d\n",count);
+JNIEXPORT void JNICALL Java_libwebcam_Webcam_lookupDevices(JNIEnv *env, jclass obj, jobject devices)
+{
+    try
+    {
+        jclass cls_dinfo = safe_FindClass( env,  "libwebcam/DeviceInfo");
+        jmethodID constructor_vinfo = safe_GetMethodID(env, cls_dinfo, "<init>", "()V");
+
+        jclass cls_list = safe_FindClass( env,  "java/util/List");
+        jmethodID list_add = safe_GetMethodID(env, cls_list, "add", "(Ljava/lang/Object;)Z");
+
+        const webcam::DeviceInfoEnumeration & enumeration = webcam::enumerator::enumerate();
+
+        const size_t count = enumeration.count();
+
+        for (size_t device_index = 0; device_index < count; device_index++) {
+            const webcam::DeviceInfo & device_info = enumeration.get(device_index);
+
+            jobject jinfo = env->NewObject(cls_dinfo,constructor_vinfo);
+
+            env->CallObjectMethod(devices,list_add,jinfo);
+        }
+
+    }
+    catch(const webcam::enumerator_exception & exc_)
+    {
+        std::cout<<exc_.what()<<std::endl;
+    }
 }
 
 }
